fcf4b8045653561355cf5717917c4c5f63cf693e
Myles Block <mylesblock@fb.com>
2022-06-17 00:29:12 -0700

    Modified further table view; ran into a movies property error

--- Flixter/MovieViewController.h
+++ Flixter/MovieViewController.h
@@ -10,6 +10,7 @@
 NS_ASSUME_NONNULL_BEGIN
 
 @interface MovieViewController : ViewController
+@property (nonatomic, strong) NSArray *myArray;
 
 @end
 
--- Flixter/MovieViewController.m
+++ Flixter/MovieViewController.m
@@ -7,7 +7,8 @@
 
 #import "MovieViewController.h"
 
-@interface MovieViewController ()
+@interface MovieViewController () <UITableViewDataSource, UITableViewDelegate>
+@property (weak, nonatomic) IBOutlet UITableView *TableView;
 
 @end
 
@@ -15,6 +16,30 @@
 
 - (void)viewDidLoad {
     [super viewDidLoad];
+    
+    self.TableView.dataSource = self;
+    self.TableView.delegate = self;
+    
+    NSURL *url = [NSURL URLWithString:@"https://api.themoviedb.org/3/movie/now_playing?api_key=66fe3a93e9f4d8ebf87b88234f2739df"];
+    //API Key changed to v3
+    NSURLRequest *request = [NSURLRequest requestWithURL:url cachePolicy:NSURLRequestReloadIgnoringLocalCacheData timeoutInterval:10.0];
+    NSURLSession *session = [NSURLSession sessionWithConfiguration:[NSURLSessionConfiguration defaultSessionConfiguration] delegate:nil delegateQueue:[NSOperationQueue mainQueue]];
+    NSURLSessionDataTask *task = [session dataTaskWithRequest:request completionHandler:^(NSData *data, NSURLResponse *response, NSError *error) {
+           if (error != nil) {
+               NSLog(@"%@", [error localizedDescription]);
+           }
+           else {
+               NSDictionary *dataDictionary = [NSJSONSerialization JSONObjectWithData:data options:NSJSONReadingMutableContainers error:nil];
+               
+//               NSLog(@"%@", dataDictionary);// log an object with the %@ formatter.
+               // TODO: Get the array of movies
+               NSArray *myArray = dataDictionary[@"keyForAnArray"];
+//               NSLog(@"%@", dataDictionary);
+               // TODO: Store the movies in a property to use elsewhere
+               // TODO: Reload your table view data
+           }
+       }];
+    [task resume];
     // Do any additional setup after loading the view.
 }
 
@@ -27,5 +52,22 @@
     // Pass the selected object to the new view controller.
 }
 */
+
+- (NSInteger)tableView:(UITableView *)tableView numberOfRowsInSection:(NSInteger)section {
+    return self.movies.count;
+}
+
+- (UITableViewCell *)tableView:(UITableView *)tableView cellForRowAtIndexPath:(NSIndexPath *)indexPath {
+    UITableViewCell *cell = [[UITableViewCell alloc] init];
+    NSDictionary *movie = self.movies[indexPath.row];
+    cell.textLabel.text = movie [@"title"];
+    
+    NSLog(@"%@", [NSString stringWithFormat:@"row: %d, section %d", indexPath.row, indexPath.section]);//prints out table row count
+    
+    cell.textLabel.text = [NSString stringWithFormat:@"row: %d, section %d", indexPath.row, indexPath.section];
+    
+    return cell;
+}
+
 
 @end
