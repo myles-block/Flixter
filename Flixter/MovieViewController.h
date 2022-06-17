650a98c131c5372614a7b02e03cdacdf1409765f
Myles Block <mylesblock@fb.com>
2022-06-17 00:01:28 -0700

    Created TableView and list of sections

---
+++ Flixter/MovieViewController.h
@@ -0,0 +1,16 @@
+//
+//  MovieViewController.h
+//  Flixter
+//
+//  Created by Myles Block on 6/16/22.
+//
+
+#import "ViewController.h"
+
+NS_ASSUME_NONNULL_BEGIN
+
+@interface MovieViewController : ViewController
+
+@end
+
+NS_ASSUME_NONNULL_END
---
+++ Flixter/MovieViewController.m
@@ -0,0 +1,31 @@
+//
+//  MovieViewController.m
+//  Flixter
+//
+//  Created by Myles Block on 6/16/22.
+//
+
+#import "MovieViewController.h"
+
+@interface MovieViewController ()
+
+@end
+
+@implementation MovieViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view.
+}
+
+/*
+#pragma mark - Navigation
+
+// In a storyboard-based application, you will often want to do a little preparation before navigation
+- (void)prepareForSegue:(UIStoryboardSegue *)segue sender:(id)sender {
+    // Get the new view controller using [segue destinationViewController].
+    // Pass the selected object to the new view controller.
+}
+*/
+
+@end
